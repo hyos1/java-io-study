065cdf66805697b43a3ea4bce2d7bbb554fe95ad
hyos <hyozzang99@gmail.com>
2025-06-23 15:26:50 +0900

    Refactor: 요청 응답 매개변수 동적 바인딩, 회원 관리 서비스 추가

--- src/was/httpserver/HttpRequest.java
+++ src/was/httpserver/HttpRequest.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.*;
+import static util.MyLogger.log;
 
 public class HttpRequest {
 
@@ -19,11 +20,13 @@ public class HttpRequest {
     public HttpRequest(BufferedReader reader) throws IOException {
         parseRequestLine(reader);
         parseHeaders(reader);
+        parseBody(reader); // 추가
         // 메세지 바디는 이후에 처리
     }
 
     // GET /search?q=hello HTTP/1.1
     // Host: localhost:12345
+
     private void parseRequestLine(BufferedReader reader) throws IOException {
         String requestLine = reader.readLine();
         if (requestLine == null) {
@@ -42,10 +45,10 @@ public class HttpRequest {
             parseQueryParameters(pathParts[1]);
         }
     }
-
     // q=hello
     // key1=value1&key2=value2
     // q=
+
     private void parseQueryParameters(String queryString) {
         for (String param : queryString.split("&")) {
             String[] keyValue = param.split("=");
@@ -54,16 +57,37 @@ public class HttpRequest {
             queryParameters.put(key, value);
         }
     }
-
     // Host: localhost:12345
     // Connection: keep-alive
     // Cache-Control: max-age=0
+
     private void parseHeaders(BufferedReader reader) throws IOException {
         String line;
         while (!(line = reader.readLine()).isEmpty()) {
             String[] headerParts = line.split(":");
             // trim() 앞 뒤 공백 제거
             headers.put(headerParts[0].trim(), headerParts[1].trim());
+        }
+    }
+
+    private void parseBody(BufferedReader reader) throws IOException {
+        if (!headers.containsKey("Content-Length")) {
+            return;
+        }
+
+        int contentLength = Integer.parseInt(headers.get("Content-Length"));
+        char[] bodyChars = new char[contentLength];
+        int read = reader.read(bodyChars);
+        if (read != contentLength) {
+            throw new IOException("Fail to read entire body. Expected " + contentLength + " bytes, but read " + read);
+        }
+        String body = new String(bodyChars);
+        log("HTTP Message Body: " + body);
+
+        String contentType = headers.get("Content-Type");
+        if ("application/x-www-form-urlencoded".equals(contentType)) {
+            // id=id1&name=name1&age=10
+            parseQueryParameters(body);
         }
     }
 
