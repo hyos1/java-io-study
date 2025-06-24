da6c9cdc0db9d28dd9af177f352166e4404f2e7b
hyos <hyozzang99@gmail.com>
2025-06-24 16:29:01 +0900

    Chore: inputStream, outputStream 복습

---
+++ src/review/io/buffered/ReadFileV4.java
@@ -0,0 +1,23 @@
+package review.io.buffered;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static review.io.buffered.BufferedConst.*;
+
+public class ReadFileV4 {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream(FILE_NAME);
+        long startTime = System.currentTimeMillis();
+
+        byte[] bytes = fis.readAllBytes();
+        fis.close();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("File created: " + FILE_NAME);
+        System.out.println("File size: " + FILE_SIZE / 1024 / 1024 + "MB");
+        System.out.println("Time taken: " + (endTime - startTime) + "ms");
+    }
+}
